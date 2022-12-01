737fc19788b4b4aad84c39bd8b676b178da3a91e
brunnohs <119610539+brunnohs@users.noreply.github.com>
2022-12-01 12:31:31 -0300

    Add files via upload

---
+++ orçamento.py
@@ -0,0 +1,22 @@
+#Projeto: Manipulando Planilhas Excel
+
+from openpyxl import Workbook
+orcamento = Workbook()
+
+Novembro = orcamento.active 
+
+Novembro["A1"] = "Contas"
+Novembro["B1"] = "Receitas"
+Novembro["C1"] = "Despesas"
+Novembro["D1"] = "Resultados"
+
+Novembro["A2"] = "Alimentação"
+Novembro["A3"] = "Energia"
+Novembro["A4"] = "Transporte"
+
+Novembro["B2"] = 1000
+Novembro["C2"] = 200
+Novembro["C3"] = 200
+Novembro["C4"] = 200
+Novembro["D2"] = Novembro["B2"].value - (Novembro["C2"].value + Novembro["C3"].value + Novembro["C4"].value) 
+orcamento.save("orcamento.xlsx")
